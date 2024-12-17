2137447131aa80c1999e3d23c873e703cfa9bb5e
bahriah <bahriahkamardi@gmail.com>
2024-12-17 19:34:59 +0800

    hii qeela nmpk tak

--- lib/page/admin/admin_appointment/admin_appointment_screen.dart
+++ lib/page/admin/admin_appointment/admin_appointment_screen.dart
@@ -33,7 +33,7 @@ class AppointmentRecordScreen extends StatelessWidget {
             SizedBox(height: 40),
             // Logo Section
             Image.asset(
-              'jarum.png',
+              'jarum.png',///hii qeela
               width: MediaQuery.of(context).size.width * 0.3, // 80% of the screen width
               height: MediaQuery.of(context).size.height * 0.2, // 40% of the screen height
               fit: BoxFit.contain, // Scales while preserving aspect ratio
